73c6c0b70e9eb68f08a5ab2c4d0a297df954bdee
Hao Ji <90297164+jihao2021@users.noreply.github.com>
2024-08-13 09:24:04 -0700

    Update mnist_classify.py

--- mnist_classify.py
+++ mnist_classify.py
@@ -124,7 +124,7 @@ def main():
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
         ])
-    dataset1 = datasets.MNIST('data/', train=True, download=False,
+    dataset1 = datasets.MNIST('data/', train=True, download=True,
                        transform=transform)
     dataset2 = datasets.MNIST('data/', train=False,
                        transform=transform)
